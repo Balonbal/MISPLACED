6b3debfe1042c4437bcdeb85f6e2ad64a14304bc
Olav Bech Bråten <olavbb@hotmail.com>
2016-04-25 17:36:25 +0200

    Started work on dialogue backend

---
+++ core/src/sexy/sly/misplaced/UI/InteractionHelper.java
@@ -0,0 +1,7 @@
+package sexy.sly.misplaced.UI;
+
+/**
+ * Created by Sly on 25.04.2016.
+ */
+public class InteractionHelper {
+}
---
+++ core/src/sexy/sly/misplaced/story/StoryParser.java
@@ -0,0 +1,7 @@
+package sexy.sly.misplaced.story;
+
+/**
+ * Created by Sly on 25.04.2016.
+ */
+public class StoryParser {
+}
